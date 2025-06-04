4cd1c0082d90bdf0b716acadca64f540dd98132e
Gili <giliolig22@gmail.com>
2025-06-03 21:41:44 -0300

    fix: app.jsx arrumado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from '@mui/material';
+// Componente principal que organiza as rotas e a barra de navegação
 import { Container } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Container sx={{ marginTop: 4 }}>
         <Routes>
+          {/* Define as rotas da aplicação */}
           <Route path="/" element={<Cadastro />} />
           <Route path="/usuarios" element={<Usuarios />} />
         </Routes>
